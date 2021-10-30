0df91c9269e5e386487709c43f932a9bc6894fc0
albinabist1993@mail.ru <b.hapilin@gateline.ru>
2021-10-30 15:29:19 +0300

    Верстка адаптива страницы "allNews";

--- ppvue/src/pages/allNews.vue
+++ ppvue/src/pages/allNews.vue
@@ -1,14 +1,14 @@
 <template lang="pug">
-    main.container.allNews-content--container
-        h1.allNews-content__h1 Новости
-        ul#newsList.news__list
-            li.news__list-item(v-for="(news, index) in allNews")
-                a.news__a(href="/news")
-                    img(:src="require(`../assets/${news.newsImg}`)")
-                    .news__description
-                        .news__wrapper--description
-                            p.news__description--p {{ news.newsP }}
-                        .news__date {{ news.newsDate }}
+    main.container.all-news-content--container
+        h1.all-news-content__h1 Новости
+        ul.all-news__list
+            li.all-news__list-item(v-for="(news, index) in allNews")
+                a(href="/news")
+                    div.all-news__img
+                    div.all-news-content--style.all-news__description-p
+                        p {{ news.newsP }}
+                    div.all-news-content--style.all-news-content--date {{ news.newsDate }}
+
 </template>
 
 <script>
@@ -33,6 +33,12 @@
                             "Тут будет подзаголовокТут будет подзаголовокТут будет подзаголовок",
                         newsDate: "29.01.2021",
                     },
+                    {
+                        newsImg: "news3.jpg",
+                        newsP:
+                            "Тут будет подзаголовокТут будет подзаголовокТут будет подзаголовок",
+                        newsDate: "29.01.2021",
+                    },
                 ],
             }
 
@@ -44,25 +50,83 @@
 <style lang="scss">
     @import "../assets/scss/general.scss";
 
+    .grid {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+    }
     main {
         color: $blackColorText;
     }
 
-    .allNews-content--container {
+    .all-news-content--container {
         padding: 55px 165px;
     }
 
-    .allNews-content__h1 {
+    /*.all-news__img {*/
+    /*    max-width: 260px;*/
+    /*    margin-bottom: 10px;*/
+    /*}*/
+
+    .all-news__img {
+        background-image: url(/img/news1.b68d0b4b.jpg);
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        width: 260px;
+        height: 150px;
+        margin-bottom: 10px;
+    }
+
+    .all-news-content__h1 {
         font-size: 34px;
         font-weight: normal;
+        margin-bottom: 20px;
     }
 
-    .news__list {
-        @include displayFlexSpaceBetween;
+    .all-news-content--style {
+        color: $blackColorText;
     }
 
-    .news__list-item {
+    .all-news__description-p {
+        margin-bottom: 5px;
+    }
+
+    .all-news-content--date {
+        font-size: 14px;
+    }
+
+    .all-news__list {
+        //@include displayFlexSpaceBetween;
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+    }
+
+    .all-news__list-item {
         width: 260px;
+    }
+
+    @media (max-width: 1380px) {
+        .all-news__list {
+            grid-template-columns: repeat(2, 1fr);
+            gap: 20px;
+        }
+
+        .all-news__list-item, .all-news__img {
+            width: 100%;
+            max-width: 100%;
+        }
+
+    }
+
+    @media (max-width: 1080px) {
+
+        .all-news-content--container {
+            padding: 30px 40px;
+        }
+
+        .all-news__list {
+            grid-template-columns: 1fr;
+        }
     }
 
 </style>
